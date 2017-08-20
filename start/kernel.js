991657c76b8a460dc5aaa5d1355d9fd3383229e2
Harminder Virk <virk.officials@gmail.com>
2017-08-20 22:39:25 +0530

    feat(*) initial commit

---
+++ start/kernel.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const Server = use('Server')
+
+/*
+|--------------------------------------------------------------------------
+| Global Middleware
+|--------------------------------------------------------------------------
+|
+| Global middleware are executed on each http request only when the routes
+| match.
+|
+*/
+const globalMiddleware = [
+  'Adonis/Middleware/BodyParser',
+  'Adonis/Middleware/Cors',
+  'Adonis/Middleware/Session',
+  'Adonis/Middleware/Shield',
+  'Adonis/Middleware/AuthInit'
+]
+
+/*
+|--------------------------------------------------------------------------
+| Named Middleware
+|--------------------------------------------------------------------------
+|
+| Named middleware is key/value object to conditionally add middleware on
+| specific routes or group of routes.
+|
+| // define
+| {
+|   auth: 'Adonis/Middleware/Auth'
+| }
+|
+| // use
+| Route.get().middleware('auth')
+|
+*/
+const namedMiddleware = {
+  auth: 'Adonis/Middleware/Auth'
+}
+
+Server
+  .registerGlobal(globalMiddleware)
+  .registerNamed(namedMiddleware)
+  .use(['Adonis/Middleware/Static'])
