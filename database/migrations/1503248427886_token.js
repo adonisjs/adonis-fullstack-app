f658a4e89588f853366b62ea2f19ece61db8b561
Alan Aasmaa <alanaasmaa@gmail.com>
2017-10-29 20:24:28 +0200

    FIX: Added type table

--- database/migrations/1503248427886_token.js
+++ database/migrations/1503248427886_token.js
@@ -8,6 +8,7 @@ class TokensSchema extends Schema {
       table.increments()
       table.integer('user_id').unsigned().references('id').inTable('users')
       table.string('token', 40).notNullable().unique()
+      table.string('type', 80).notNullable()
       table.boolean('is_revoked').defaultTo(false)
       table.timestamps()
     })
