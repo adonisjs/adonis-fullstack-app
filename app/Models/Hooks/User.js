b86de22ffc5a88e7cc3c3bd69ef52e9804cc0ea8
Marco Vettorello <vettorello.marco@gmail.com>
2017-11-09 12:51:32 +0100

    Fix missing dirty check
    If not dirty checking the password field, rehashing hashed password take place

--- app/Models/Hooks/User.js
+++ app/Models/Hooks/User.js
@@ -14,7 +14,7 @@ const UserHook = module.exports = {}
  * @return {void}
  */
 UserHook.hashPassword = async (userInstance) => {
-  if (userInstance.password) {
+  if (userInstance.dirty.password) {
     userInstance.password = await Hash.make(userInstance.password)
   }
 }
