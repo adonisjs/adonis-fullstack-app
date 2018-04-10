6a18c62d31e570509f8334007004d715185006ba
Romain Lanz <romain.lanz@slynova.ch>
2018-04-10 17:21:12 +0200

    chore(auth): remove deprecated call
    Closes https://github.com/adonisjs/adonis-fullstack-app/issues/17

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Env = use('Env')
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -69,7 +71,7 @@ module.exports = {
     uid: 'email',
     password: 'password',
     options: {
-      secret: 'self::app.appKey'
+      secret: Env.get('APP_KEY')
     }
   }
 }
