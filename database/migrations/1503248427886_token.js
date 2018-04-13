43198c1814fc45e340dde68d8002ee4e97595bc1
Kevyworks <kevmt@users.noreply.github.com>
2018-04-13 15:10:34 +0800

    chore(schema): Change token length + index

--- database/migrations/1503248427886_token.js
+++ database/migrations/1503248427886_token.js
@@ -7,7 +7,7 @@ class TokensSchema extends Schema {
     this.create('tokens', table => {
       table.increments()
       table.integer('user_id').unsigned().references('id').inTable('users')
-      table.string('token', 40).notNullable().unique()
+      table.string('token', 255).notNullable().unique().index()
       table.string('type', 80).notNullable()
       table.boolean('is_revoked').defaultTo(false)
       table.timestamps()
