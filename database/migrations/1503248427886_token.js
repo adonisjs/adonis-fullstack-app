d5cb3aee4b42a9f173e3b1a0b82bc3d72f8de743
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-04-30 09:56:26 -0300

    Update 1503248427886_token.js

--- database/migrations/1503248427886_token.js
+++ database/migrations/1503248427886_token.js
@@ -4,7 +4,7 @@ const Schema = use('Schema')
 
 class TokensSchema extends Schema {
   up () {
-    this.create('tokens', table => {
+    this.create('tokens', (table) => {
       table.increments()
       table.integer('user_id').unsigned().references('id').inTable('users')
       table.string('token', 255).notNullable().unique().index()
