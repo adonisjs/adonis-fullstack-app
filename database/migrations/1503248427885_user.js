550bcd891d89ad54f77a518a660569ecf7c8ed1d
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-05-21 18:37:14 -0300

    style(migration): use parentheses in arrow function
    * Update 1503248427885_user.js
    * Update 1503248427886_token.js

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -4,7 +4,7 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', table => {
+    this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
