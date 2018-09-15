dc6f64c1b21eaf6da92c92d626766431484ac3d7
Romain Lanz <romain.lanz@slynova.ch>
2018-09-15 09:43:08 +0200

    feat(intellisense): add type comments

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
 module.exports = {
--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 'use strict'
 
+/** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
+
+/** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
 module.exports = {
