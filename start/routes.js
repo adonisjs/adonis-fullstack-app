1d002c47d3d4e9b14a30e9c62c17615e8ef333e3
Johnny Wu <wu-xuesong@qq.com>
2018-10-02 02:23:03 +0800

    fix type of Route (#31)

--- start/routes.js
+++ start/routes.js
@@ -13,7 +13,7 @@
 |
 */
 
-/** @type {import('@adonisjs/framework/src/Route/Manager'} */
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager'} */
 const Route = use('Route')
 
 Route.on('/').render('welcome')
