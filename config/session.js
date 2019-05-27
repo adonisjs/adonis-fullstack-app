2112a60001b9e6fbcabca604ab7ee17fd7005c99
Zane Chua <4265429+zanechua@users.noreply.github.com>
2019-05-27 15:42:37 +0800

    Update session.js redis configuration (#42)

--- config/session.js
+++ config/session.js
@@ -91,5 +91,11 @@ module.exports = {
   | the redis file. But you are free to define an object here too.
   |
   */
-  redis: 'self::redis.local'
+  redis: {
+    host: '127.0.0.1',
+    port: 6379,
+    password: null,
+    db: 0,
+    keyPrefix: ''
+  }
 }
