c99954bbbface7938e7d8c935bccae74982d5c66
vuurball <oneindigedroom@gmail.com>
2020-01-24 16:26:54 +0200

    Update factory.js
    added required fields, to match the database table constraints which is made by the built-in seeder.
    this might help new comers save time figuring out

--- database/factory.js
+++ database/factory.js
@@ -13,9 +13,12 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 // const Factory = use('Factory')
+// const Hash = use('Hash')
 
-// Factory.blueprint('App/Models/User', (faker) => {
+// Factory.blueprint('App/Models/User', async (faker) => {
 //   return {
-//     username: faker.username()
+//     username: faker.username(),
+//     email: faker.email(),
+//     password: await Hash.make(faker.password())
 //   }
 // })
