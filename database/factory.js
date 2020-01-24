9a34b80959bb26450f0464b7dd060a3815ad3b25
vuurball <oneindigedroom@gmail.com>
2020-01-24 17:12:04 +0200

    Update factory.js
    removed the hashing of a password because it's done behind the scenes

--- database/factory.js
+++ database/factory.js
@@ -15,10 +15,10 @@
 // const Factory = use('Factory')
 // const Hash = use('Hash')
 
-// Factory.blueprint('App/Models/User', async (faker) => {
+// Factory.blueprint('App/Models/User', (faker) => {
 //   return {
 //     username: faker.username(),
 //     email: faker.email(),
-//     password: await Hash.make(faker.password())
+//     password: faker.password()
 //   }
 // })
